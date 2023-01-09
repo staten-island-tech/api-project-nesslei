dd7f7580d26778cafe53b9fdc8ce9e02c65372af
nesslei <botchedbunnies@gmail.com>
2023-01-09 13:57:11 -0500

    r57,r5r
    t8r7

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,22 @@
 import "./style.css";
+
+const URL = "https://api.quotable.io/randoms";
+
+async function getData(URL) {
+  try {
+    const resopnse = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      console.timeLog(reospnse.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+
+      console.log(data.data[0].entry);
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("whatever");
+    document.getElementById("API-RESPONSE").textContent = "mommy";
+  }
+}
+getData(URL);
