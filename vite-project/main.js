a857852545e5f20e2b09e59b71c5e442c32ac9d7
nesslei <botchedbunnies@gmail.com>
2023-01-12 14:01:33 -0500

    vawvdawv
    ewvav

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,29 +1,31 @@
 import "./style.css";
 
-const games = "https://www.freetogame.com/api/games";
+const pets =
+  "http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]";
 
-async function getData(games) {
+async function getData(pets) {
   try {
-    const response = await fetch(games);
+    const response = await fetch(pets);
     if (response.status < 200 || response.status > 299) {
       console.Log(response.status);
       throw error(response);
     } else {
       const data = await response.json();
 
-      data.results.forEach((games) => {
+      data.forEach((pets) => {
         document.getElementById("display").insertAdjacentHTML(
           "afterbegin",
           `<div class = "card"
-          <h1> "${games.title}"</h1>
-          <img src=${games.thumbnail} alt = "">       
+          <h1> ${pets.title}</h1>
+          <img src="${pets.thumbnail}" alt = "">       
           </div>`
         );
       });
     }
   } catch (error) {
-    console.log("error");
+    console.log(error);
+    console.log("u suck");
     document.getElementById("display").textContent = "u suck";
   }
 }
-getData(URL);
+getData(pets);
