259c36e71375ee90140cff17765430ff2537c29e
VanesssaLei <botchedbunnies@gmail.com>
2023-01-15 18:42:15 -0500

    poop
    io pooped my pants

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,2 +1,26 @@
 import "./style.css";
 
+const URL = "https://deezerdevs-deezer.p.rapidapi.com/artist/The%20Weeknd";
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '868afab899msh18229938fd806e0p1740e9jsn539d4b22a966',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
+
+async function getData(URL) {
+  try {
+    const response = await fetch (
+      "https://deezerdevs-deezer.p.rapidapi.com/artist/The%20Weeknd",
+      options
+    ); 
+    const data = await response.json(); 
+    console.log(data); 
+    } catch (error) {
+      console.log(error); 
+    }
+  }
+  getData(URL); 
+
