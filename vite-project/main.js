5b03054f0b677f89606857bfe31640689b050a51
VanesssaLei <botchedbunnies@gmail.com>
2023-01-15 22:49:55 -0500

    push

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,19 +1,68 @@
 import "./style.css";
 
-const URL = "https://deezerdevs-deezer.p.rapidapi.com/artist/The%20Weeknd";
+const url = 'https://deezerdevs-deezer.p.rapidapi.com/album/14652356';
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '868afab899msh18229938fd806e0p1740e9jsn539d4b22a966',
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '82720bd7d6msh2d19c07fd7db3ddp1297f3jsn24bf33fef3b4',
+    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+  }
 };
 
-async function getData(URL) {
+fetch(url, options)
+    .then(res => res.json())
+    .then(json => { 
+
+      console.log(json)
+      json.tracks.data.forEach((track) => {
+        document.getElementById("display").insertAdjacentHTML(
+          "afterbegin", 
+          `<div class = "card" >
+          <img src="${json.cover}" alt = "">
+          <h2> ${json.title} </h2> 
+          <h3> ${track.title} </h3> 
+          <audio controls  src=${track.preview}>
+          </div>
+          `
+        )
+      })
+})
+
+    .catch(err => console.error('error:' + err));
+
+
+/*const URL = "https://api.deezer.com/album/14652356";
+
+async function getData(URL) { 
   try {
+    const response = await fetch (URL); 
+    if(response.status < 200 || response.status > 299) {
+      console.log(response.status); 
+      throw error(response); 
+    } else {
+      const data = await response.json(); 
+
+      data.results.forEach((song) => {
+        document.getElementById("display").insertAdjacentHTML(
+          "afterbegin", 
+          `div class = "card" >
+          <img src="${song.url}" alt = "">
+          </div>
+          `
+        );
+      });
+    }
+    } catch (error) {
+      console.log(error); 
+      console.log("u suck"); 
+  }
+}
+  getData(URL); 
+
+  /*  try {
     const response = await fetch (
-      "https://deezerdevs-deezer.p.rapidapi.com/artist/The%20Weeknd",
+      "https://api.deezer.com/album/14652356",
       options
     ); 
     const data = await response.json(); 
@@ -22,5 +71,4 @@ async function getData(URL) {
       console.log(error); 
     }
   }
-  getData(URL); 
-
+  getData(URL); */ 
